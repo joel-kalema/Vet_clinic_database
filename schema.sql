af8b789dd8e0968315662656744248f913a6eecc
joel <joelkalema63@gmail.com>
2022-06-08 08:01:40 +0200

    Add datas

---
+++ schema.sql
@@ -0,0 +1,10 @@
+/* Database schema to keep the structure of entire database. */
+
+CREATE TABLE animals(
+	id BIGSERIAL NOT NULL PRIMARY KEY,
+	name VARCHAR UNIQUE NOT NULL, 
+	date_of_birth DATE, 
+	escape_attempts INTEGER, 
+	neutered BOOLEAN, 
+	weight_kg DECIMAL
+);
